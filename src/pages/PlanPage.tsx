ce68f4c84cfd24fbf2153f6cecee2a6ac45094a1
jlsuh <joelleonardosuh@gmail.com>
2023-12-31 22:35:11 -0300

    refactor: delete unnecessary isReady state

--- src/pages/PlanPage.tsx
+++ src/pages/PlanPage.tsx
@@ -1,20 +1,20 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect } from "react";
 import { PlanContext } from "../context";
 import { PlanLayout } from "../layout";
 import { Plan, PlanContextProps } from "../types/types";
 import { PlanView } from "../views";
 
 export const PlanPage = ({ plan }: { plan: Plan }) => {
-  const [isReady, setIsReady] = useState(false);
   const { contextPlan, setContextPlan } =
     useContext<PlanContextProps>(PlanContext);
 
   useEffect(() => {
-    if (contextPlan.id !== plan.id) {
-      setContextPlan(plan);
-    }
-    setIsReady(true);
+    if (contextPlan.id !== plan.id) setContextPlan(plan);
   }, [contextPlan.id, plan, setContextPlan]);
 
-  return <PlanLayout>{isReady && <PlanView />}</PlanLayout>;
+  return (
+    <PlanLayout>
+      <PlanView />
+    </PlanLayout>
+  );
 };
