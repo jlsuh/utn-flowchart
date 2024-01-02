a5bb9a482997b0e4f7a74107320c94322591b466
jlsuh <joelleonardosuh@gmail.com>
2024-01-02 16:49:45 -0300

    refactor: delete unnecessary setContextPlan

--- src/context/PlanContext.tsx
+++ src/context/PlanContext.tsx
@@ -3,7 +3,6 @@ import { Plan, PlanContextProps } from "../types/types";
 
 export const PlanContext = createContext<PlanContextProps>({
   contextPlan: {} as Plan,
-  setContextPlan: () => {},
   updateMode: () => {},
   updateStatuses: () => {},
 });
--- src/pages/PlanPage.tsx
+++ src/pages/PlanPage.tsx
@@ -1,17 +1,7 @@
-import { useContext, useEffect } from "react";
-import { PlanContext } from "../context";
 import { PlanLayout } from "../layout";
-import { Plan, PlanContextProps } from "../types/types";
 import { PlanView } from "../views";
 
-export const PlanPage = ({ plan }: { plan: Plan }) => {
-  const { contextPlan, setContextPlan } =
-    useContext<PlanContextProps>(PlanContext);
-
-  useEffect(() => {
-    if (contextPlan.id !== plan.id) setContextPlan(plan);
-  }, [contextPlan.id, plan, setContextPlan]);
-
+export const PlanPage = () => {
   return (
     <PlanLayout>
       <PlanView />
