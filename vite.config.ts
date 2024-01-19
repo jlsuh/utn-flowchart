1415b9bb382d8d0d15e880aa46dec4558c13bd2d
jlsuh <joelleonardosuh@gmail.com>
2024-01-19 11:54:37 -0300

    chore: export plain object as config & include globs for test files

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
-export default defineConfig(() => ({
+export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
     environment: "jsdom",
+    include: ["src/**/*.{test,spec}.{js,ts,tsx}"],
   },
-}));
+});
