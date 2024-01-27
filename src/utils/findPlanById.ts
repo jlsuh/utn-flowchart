4d22b3c3dda3e444fbb42a4a603ae516155ecd8f
jlsuh <joelleonardosuh@gmail.com>
2024-01-26 21:53:04 -0300

    refactor: extract throw into findPlandById

--- src/utils/findPlanById.ts
+++ src/utils/findPlanById.ts
@@ -1,5 +1,10 @@
 import { plans } from "../data";
 
 export const findPlanById = (id: string) => {
-  return plans.find((plan) => plan.id === id);
+  return (
+    plans.find((plan) => plan.id === id) ??
+    (() => {
+      throw new Error(`Plan with id "${id}" not found`);
+    })()
+  );
 };
