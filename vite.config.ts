74b869d2a7269ebd97bbb9cafaf535d836bb72cd
jlsuh <joelleonardosuh@gmail.com>
2024-02-02 19:32:55 -0300

    build: extract vitest configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,6 @@
-/// <reference types="vitest" />
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: "jsdom",
-    include: ["src/**/*.{test,spec}.{js,ts,tsx}"],
-  },
 });
