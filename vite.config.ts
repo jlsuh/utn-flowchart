a36a5f4f2831e0d1eda51c2775002a8f9f684bd8
jlsuh <joelleonardosuh@gmail.com>
2024-02-03 23:24:01 -0300

    build: extract manual chunks function

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,19 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
+const NODE_MODULES = "node_modules";
+
+const manualChunks = (id: string) =>
+  id.indexOf(NODE_MODULES) !== -1
+    ? id.toString().split(`${NODE_MODULES}/`)[1].split("/")[0].toString()
+    : undefined;
+
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
       output: {
-        manualChunks(id) {
-          if (id.indexOf("node_modules") !== -1) {
-            return id
-              .toString()
-              .split("node_modules/")[1]
-              .split("/")[0]
-              .toString();
-          }
-        },
+        manualChunks,
       },
     },
   },
