e08b25eb497381ab9f33a45c3a680283e35d371e
Cristian Liu <cris664@github.com>
2024-02-29 21:24:40 -0300

    import r23

--- src/data/plans.ts
+++ src/data/plans.ts
@@ -1,5 +1,7 @@
 import { DataPlan } from "../types/types";
 import { k08 } from "./k08";
 import { k23 } from "./k23";
+import { r23 } from "./r23";
 
-export const plans: ReadonlyArray<DataPlan> = [k08, k23];
+
+export const plans: ReadonlyArray<DataPlan> = [k08, k23,r23];
