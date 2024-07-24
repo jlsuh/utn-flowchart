576ed4c595fe7d4d12003f906cf8a7c56a91f4c9
jlsuh <joelleonardosuh@gmail.com>
2024-07-24 08:24:33 -0300

    feat: add spinner

--- src/components/ShowDigraphButton.tsx
+++ src/components/ShowDigraphButton.tsx
@@ -2,7 +2,8 @@ import { useDigraph } from '@/hooks';
 import { renderSVG } from '@/services';
 import { composeSVGObjectURL } from '@/utils';
 import PolylineIcon from '@mui/icons-material/Polyline';
-import { Button, Typography } from '@mui/material';
+import { Button, CircularProgress, Typography } from '@mui/material';
+import { useState } from 'react';
 
 const continuation = (svg: SVGElement) => {
   const url = composeSVGObjectURL(svg);
@@ -15,16 +16,24 @@ const continuation = (svg: SVGElement) => {
 };
 
 function ShowDigraphButton() {
+  const [isLoading, setIsLoading] = useState(false);
+
   const { composeDigraph } = useDigraph();
 
-  const handleClickShowDigraph = () => {
+  const handleClickShowDigraph = async () => {
+    setIsLoading(true);
     const digraph = composeDigraph();
-    renderSVG(digraph, continuation);
+    await renderSVG(digraph, continuation);
+    setIsLoading(false);
   };
 
   return (
     <Button variant="outlined" onClick={handleClickShowDigraph}>
-      <PolylineIcon sx={{ mr: 1, fontSize: 22 }} />
+      {isLoading ? (
+        <CircularProgress sx={{ mr: 1 }} size={22} />
+      ) : (
+        <PolylineIcon sx={{ mr: 1, fontSize: 22 }} />
+      )}
       <Typography>Ver Digrafo</Typography>
     </Button>
   );
