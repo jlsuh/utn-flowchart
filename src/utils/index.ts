aacd7b4044a88ec1a8ffaf738e1af2ec3a9dc16e
jlsuh <joelleonardosuh@gmail.com>
2024-07-27 00:43:18 -0300

    chore: reorder barrel

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,4 @@
-export { default as Digraph } from './Digraph';
 export { default as composeSVGObjectURL } from './composeSVGObjectURL';
+export { default as Digraph } from './Digraph';
 export { default as findPlanById } from './findPlanById';
 export { default as replaceWhiteSpace } from './replaceWhiteSpace';
