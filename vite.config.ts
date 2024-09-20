bed8849a46b6ffca84f4f963d4c9a8b95e8ca1ac
jlsuh <joelleonardosuh@gmail.com>
2024-09-20 01:35:56 -0300

    fix: specify load order for manual chunks

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,18 @@ import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-const NODE_MODULES = 'node_modules';
-
-const manualChunks = (id: string) =>
-  id.indexOf(NODE_MODULES) !== -1
-    ? id.toString().split(`${NODE_MODULES}/`)[1].split('/')[0].toString()
-    : undefined;
-
 const viteConfig = defineConfig({
   plugins: [react(), tsconfigPaths()],
   build: {
     rollupOptions: {
       output: {
-        manualChunks,
+        manualChunks: {
+          reactDOM: ["react-dom"],
+          emotionReact: ["@emotion/react"],
+          emotionStyled: ["@emotion/styled"],
+          muiMaterial: ["@mui/material"],
+          viz: ["@viz-js/viz"],
+        },
       },
     },
   },
