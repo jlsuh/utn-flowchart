3b68230bd8d09b15338e26e06806e5325fa45c81
jlsuh <joelleonardosuh@gmail.com>
2026-03-25 14:05:52 -0300

    chore: k23 as initial plan

--- src/data/plans.ts
+++ src/data/plans.ts
@@ -3,6 +3,6 @@ import k08 from './k08';
 import k23 from './k23';
 import r23 from './r23';
 
-const plans: ReadonlyArray<DataPlan> = [k08, k23, r23];
+const plans: ReadonlyArray<DataPlan> = [k23, r23, k08];
 
 export default plans;
